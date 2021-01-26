0efd2f4712368024df03f8ca452d6b066acaffd6
Charles Naut <cpn585@gmail.com>
2021-01-26 16:57:42 -0500

    Implement drag and drop for button component

--- pages/builder.js
+++ pages/builder.js
@@ -9,7 +9,10 @@ import {
   Stack,
   TextField,
   TextStyle,
+  DisplayText
 } from '@shopify/polaris';
+import { useDrag, useDrop, DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 import store from 'store-js';
 
 class Builder extends React.Component {
@@ -20,34 +23,92 @@ class Builder extends React.Component {
       components: [],
       componentsConfig: []
     }
+
+    this.ItemTypes = {
+      BUTTON: 'button'
+    }
+  }
+
+  ButtonComponent = () => {
+    const [{ isDragging }, drag] = useDrag({
+      item: { type: this.ItemTypes.BUTTON },
+      collect: (monitor) => ({
+        isDragging: !!monitor.isDragging()
+      })
+    })
+    
+    return (
+      <div className="component-div">
+        <p>Button</p>
+        <div
+          ref={drag}
+          style={{
+            opacity: isDragging ? 0.5 : 1,
+            fontSize: 25,
+            fontWeight: 'bold',
+            cursor: 'move',
+          }}
+        >
+          <Button 
+            onClick={this.addButton}
+          >
+            Example button
+          </Button>
+        </div>
+
+      </div>
+    )
+  }
+
+  PreviewComponent = () => {
+    const [, drop] = useDrop({
+      accept: this.ItemTypes.BUTTON,
+      drop: () => this.addButton()
+    })
+
+    return (
+      <div
+        ref={drop}
+        className="preview-div"
+      >
+        { this.state.components.map((component) =>  (
+            component
+          ))
+        }
+      </div>
+    );
   }
   
   render() {
     return (
-      <div>
-        <div className="builder-div">
-          <div>
-            <p>Text Field</p>
-            <TextField
-              value={this.state.title}
-              onChange={this.handleChange}
-            />
+      <DndProvider backend={HTML5Backend}>
+        <div className="builder-container">
+          <div className="builder-div">
+            <h2>Components</h2>
+            
+            <div className="component-div">
+              <p>Text Field</p>
+              <TextField
+                value={this.state.title}
+                onChange={this.handleChange}
+              />
+            </div>
+
+            <hr />
+            
+            <this.ButtonComponent />
+
+            <hr />
+
+            <div className="component-div">
+              <DisplayText size="large">Display Text</DisplayText>
+            </div>
           </div>
 
-          <div>
-            <p>Button</p>
-            <Button onClick={this.addButton}>Example button</Button>
-          </div>
-        </div>
+          <this.PreviewComponent />
 
-        <div className="preview-div">
-          { this.state.components.map((component) =>  (
-              component
-            ))
-          }
         </div>
-
-      </div>
+      </DndProvider>
     );
   }
 
@@ -59,6 +120,7 @@ class Builder extends React.Component {
   };
 
   addButton = () => {
+    console.log("ADDING")
     let components = [...this.state.components]
     components.push(<Button onClick={this.addButton}>Example button</Button>)
 
